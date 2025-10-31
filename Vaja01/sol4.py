26c5bf8421d4698d5bb012349a6b620aaa0dca61
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2025-10-31 18:18:57 +0100

    4 do c)

--- Vaja01/sol4.py
+++ Vaja01/sol4.py
@@ -1,6 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib
+import math
+import numpy as np
 
 ### a)
 
@@ -29,33 +31,91 @@ elif B == C_high:
 H = 1/6 * (H_0 + 6) if H_0 < 0 else 1/6 * H_0
 
 print(f"RGB ({R}, {G}, {B}) = HSV ({H}, {S}, {V})")
-print("HSV z matplotlib funkcijo:", matplotlib.colors.rgb_to_hsv([R/255, G/255, B/255]))
+print("RGB v HSV z matplotlib funkcijo:", matplotlib.colors.rgb_to_hsv([R/255, G/255, B/255]))
 print()
 
+### b)
 
+H = 0.65
+S = 0.7
+V = 0.15
 
-# 1. Branje slike
+H_0 = (6.0 * H) % 6
+c1 = math.floor(H_0)
+c2 = H_0 - c1
+
+x = V * (1.0 - S)
+y = (1.0 - (S * c2)) * V
+z = (1.0 - S * (1 - c2)) * V
+
+if c1 == 0:
+    R_0, G_0, B_0 = (V, z, x)
+elif c1 == 1:
+    R_0, G_0, B_0 = (y, V, x)
+elif c1 == 2:
+    R_0, G_0, B_0 = (x, V, z)
+elif c1 == 3:
+    R_0, G_0, B_0 = (x, y, V)
+elif c1 == 4:
+    R_0, G_0, B_0 = (z, x, V)
+elif c1 == 5:
+    R_0, G_0, B_0 = (V, x, y)
+
+N = 255
+
+R = min(round(N * R_0), N)
+G = min(round(N * G_0), N)
+B = min(round(N * B_0), N)
+    
+print(f"HSV ({H}, {S}, {V}) = RGB ({R}, {G}, {B})")
+print("HSV v RGB z matplotlib funkcijo:", matplotlib.colors.hsv_to_rgb([H, S, V]) * 255)
+print()
+
+### c)
+
 img = cv2.imread('slike/trucks.jpg')
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# 2. Prikaz RGB in posameznih kanalov
-plt.figure(figsize=(10,6))
-plt.subplot(2,2,1); plt.imshow(img_rgb); plt.title('RGB slika')
-plt.subplot(2,2,2); plt.imshow(img_rgb[:,:,0], cmap='gray'); plt.title('R kanal')
-plt.subplot(2,2,3); plt.imshow(img_rgb[:,:,1], cmap='gray'); plt.title('G kanal')
-plt.subplot(2,2,4); plt.imshow(img_rgb[:,:,2], cmap='gray'); plt.title('B kanal')
+plt.subplot(2,2,1)
+plt.title('RGB slika')
+plt.imshow(img_rgb)
+
+plt.subplot(2,2,2)
+plt.imshow(img_rgb[...,0], cmap='gray')
+plt.title('R kanal')
+
+plt.subplot(2,2,3)
+plt.imshow(img_rgb[...,1], cmap='gray')
+plt.title('G kanal')
+
+plt.subplot(2,2,4)
+plt.imshow(img_rgb[...,2], cmap='gray')
+plt.title('B kanal')
+
 plt.show()
 
-# 3. Pretvorba v HSV
+# pretvorba v HSV
 img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
 
-# 4. Prikaz HSV komponent
-plt.figure(figsize=(10,6))
-plt.subplot(2,2,1); plt.imshow(img_hsv[:,:,0], cmap='gray'); plt.title('H kanal')
-plt.subplot(2,2,2); plt.imshow(img_hsv[:,:,1], cmap='gray'); plt.title('S kanal')
-plt.subplot(2,2,3); plt.imshow(img_hsv[:,:,2], cmap='gray'); plt.title('V kanal')
+plt.subplot(2,2,1)
+plt.title('RGB slika')
+plt.imshow(img_rgb)
+
+plt.subplot(2,2,2)
+plt.imshow(img_hsv[...,0] / 180.0, cmap='gray') # COLOR_RGB2HSV naredi H na intervalu [0,180]
+plt.title('H kanal')
+
+plt.subplot(2,2,3)
+plt.imshow(img_hsv[...,1], cmap='gray')
+plt.title('S kanal')
+
+plt.subplot(2,2,4)
+plt.imshow(img_hsv[...,2], cmap='gray')
+plt.title('V kanal')
+
 plt.show()
 
+### d)
 # Modri kanal
 blue = img_rgb[:,:,2]
 
@@ -68,8 +128,16 @@ hue = img_hsv[:, :, 0]
 blue_thres_hsv = (hue >= 90) & (hue <= 130)
 
 # Prikaz
-plt.figure(figsize=(10,5))
-plt.subplot(1,3,1); plt.imshow(img_rgb); plt.title('Original')
-plt.subplot(1,3,2); plt.imshow(blue_thres, cmap='gray'); plt.title('Upragovana slika (B>150)')
-plt.subplot(1,3,3); plt.imshow(blue_thres_hsv, cmap='gray'); plt.title('Upragovana slika (100<H<130)')
+plt.subplot(1,3,1)
+plt.imshow(img_rgb)
+plt.title('Original')
+
+plt.subplot(1,3,2)
+plt.imshow(blue_thres, cmap='gray')
+plt.title('Upragovana slika (B>150)')
+
+plt.subplot(1,3,3)
+plt.imshow(blue_thres_hsv, cmap='gray')
+plt.title('Upragovana slika (100<H<130)')
+
 plt.show()
