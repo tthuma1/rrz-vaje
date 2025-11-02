a33bf11aa999c965dc8bd13067968315856f4524
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2025-11-02 13:37:21 +0100

    2d)

--- Vaja01/sol2.py
+++ Vaja01/sol2.py
@@ -68,9 +68,12 @@ x = px_count / DPI * 2.54 * 10
 X = x / f * Z
 X_v_metrih = X / 1000
 
+print()
 print(f"Višina valja: {X_v_metrih:.3f}")
 
 ### d)
+
+print()
 
 # f = x * Z / X
 fs = []
@@ -78,19 +81,45 @@ fs = []
 # Realne višina objekta: 15.4 cm
 X = 15.4
 
-# 36 cm -> 458 px
-
-fs.append(458 * 36 /)
+# 36 cm -> 459 px
+fs.append(459 * 36 / X)
 
 # 47 cm -> 366 px
+fs.append(366 * 47 / X)
 
 # 55.5 cm -> 312 px
+fs.append(312 * 55.5 / X)
 
 # 64 cm -> 276 px
+fs.append(276 * 64 / X)
 
 # 75 cm -> 240 px
+fs.append(240 * 75 / X)
 
 # 86.5 cm -> 210 px
+fs.append(210 * 86.5 / X)
 
-# ----------------
+f_avg = np.mean(fs)
+print("Ocenjene goriščne razdalje [px]:", fs)
+print("Povprečna goriščna razdalja [px]:", f_avg)
+print("Povprečna napaka [px]:", np.array([abs(f_avg - f) for f in fs]))
+print("Povprečna napaka [%]:", np.array([abs(f_avg - f) / f_avg * 100 for f in fs]))
+
+# ---------------- testi -------------
+
+# 58.5 cm -> 303 px
+print()
+ocena = f_avg * X / 58.5
+real = 303
+print("Ocena 58.5 cm ->", round(ocena, 2), "px")
+print("Realno 58.5 cm ->", real, "px")
+print("Napaka:", round(abs(real - ocena), 2), "px =", round(abs(real - ocena) / real * 100, 1), "%")
+
+# 115 cm -> 162 px
+print()
+ocena = f_avg * X / 115
+real = 162
+print("Ocena 115 cm ->", round(ocena, 2), "px")
+print("Realno 115 cm ->", real, "px")
+print("Napaka:", round(abs(real - ocena), 2), "px =", round(abs(real - ocena) / real * 100, 1), "%")
 
