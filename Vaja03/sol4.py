840fdca5139d95f37330605d224667e5b3dc69d3
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2026-01-04 14:51:22 +0100

    added stanford ccd example

--- Vaja03/sol4.py
+++ Vaja03/sol4.py
@@ -138,9 +138,87 @@ def sol4d_plot():
 
 	q_sol, success, iters, error = ik_ccd(target, q0, dh_params, tol=1e-5, max_iter=10, do_plot=True)
 
+def sol4d_stanford():
+	global stopped
+
+	# Link lengths
+	L1 = 0.5  # base height
+	L2 = 0.4
+	L3 = 0.3
+
+	# stanford RRP robot
+	dh_params = [
+		{"a": 0.0, "alpha": -np.pi/2, "d": L1, "theta_offset": 0.0, "joint_type": "r"},
+		{"a": 0.0, "alpha": np.pi/2,  "d": L2, "theta_offset": 0.0, "joint_type": "r"},
+		{"a": 0.0, "alpha": 0.0,      "d_offset": L3, "theta_offset": 0.0, "joint_type": "p"},
+	]
+
+	stopped = False
+
+	# stop with 'q' button
+	def on_press(event):
+		global stopped
+		if event.key == 'q':
+			stopped = True
+
+	# Choose a target position inside the reachable workspace
+	target = np.array([0.4, 0.1, 0.6])
+
+	# Initial guess for joint angles (radians)
+	q0 = np.random.random(3)
+
+	# set up figure
+	fig = plt.figure(figsize=(8,6))
+	ax = fig.add_subplot(111, projection='3d')
+	fig.canvas.mpl_connect('key_press_event', on_press)
+
+	# generate points
+	n_pts = 20
+	width = 0.5
+	pts = generate_square(width=width, N=n_pts, target_position=target)
+	max_iter = 50
+
+	# infinite loop
+	while not stopped:
+
+		# iterate through points
+		for target in pts:
+			if stopped:
+				break
+
+			# solve for position
+			q_sol, success, iters, error = ik_ccd(target, q0, dh_params, max_iter=max_iter)
+			# get joint positions for the solution
+			_, positions = end_effector_pos(q_sol, dh_params)
+			q0 = q_sol
+
+			# plot robot positions
+			ax.clear()
+			ax.set_xlim([-1, 1])
+			ax.set_ylim([-1, 1])
+			ax.set_zlim([0, 2])
+			plt.plot(0,0, 0, 'k*')
+			plt.plot(target[0],target[1], target[2], 'b*')
+			plt.plot(pts[:,0],pts[:,1], pts[:,2], 'm.')
+			plt.title(f'error: {error:.3f} in {iters} iterations')
+
+			for i, T in enumerate(positions):
+				x,y,z = T[:3, -1]
+
+				if i>0:
+					T_ = positions[i-1]
+					x_,y_,z_ = T_[:3, -1]
+					plt.plot([x,x_],[y,y_],[z,z_], 'r-')
+				else:
+					plt.plot([0,x],[0,y],[0,z], 'r-')
+
+				plt.plot(x,y, z, 'r.')
+			plt.draw(); plt.pause(0.01)
+
 if __name__ == "__main__":
 	# sol4a()
 	# sol4c()
 	# sol4d()
-	sol4d_plot()
+	# sol4d_plot()
+	sol4d_stanford()
 
