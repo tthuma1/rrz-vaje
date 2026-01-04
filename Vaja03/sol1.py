f5d2ebedb6d1dba5695511c430fa3abb067c25ee
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2026-01-04 17:23:10 +0100

    added pozicije sklepov test to sol1

--- Vaja03/sol1.py
+++ Vaja03/sol1.py
@@ -32,6 +32,25 @@
 #        |
 #     ___|___
 
+from lerobot.robots.so101_follower import SO101Follower, SO101FollowerConfig
+from pathlib import Path
+
+arm_name = 'arm_f4'
+port = f'/dev/{arm_name}'
+calibration_dir='calibrations'
+robot_config = SO101FollowerConfig(port=port, id=arm_name,
+calibration_dir=Path(calibration_dir))
+robot = SO101Follower(robot_config)
+
+robot.connect() # povezava na robota
+robot.bus.disable_torque() # ugasnemo motorje
+
+while True:
+    current_obs = robot.get_observation()
+    print(f'{current_obs=}')
+
+robot.disconnect()
+
 ### b)
 
 # Koliko natančno lahko kontrolirate pozicijo roke? Razmislite, kakšne so omejitve sklepov robota, ki ga
