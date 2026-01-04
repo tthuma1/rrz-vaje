52840b3c062f87d42d19f13f71b78d820ea434cd
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2026-01-04 19:34:25 +0100

    added 5b examples

--- Vaja03/sol5a.py
+++ Vaja03/sol5a.py
@@ -14,72 +14,70 @@ from ikpy.utils import geometry
 from utils import *
 
 def IK_demo():
-    global stopped
+	global stopped
 
-    stopped = False
+	stopped = False
 
-    # stop with 'q' button
-    def on_press(event):
-        global stopped
-        if event.key == "q":
-            stopped = True
+	# stop with 'q' button
+	def on_press(event):
+		global stopped
+		if event.key == "q":
+			stopped = True
 
-    URDF_PATH = "so101_new_calib.urdf"
+	URDF_PATH = "so101_new_calib.urdf"
 
-    my_chain = ikpy.chain.Chain.from_urdf_file(URDF_PATH)
+	my_chain = ikpy.chain.Chain.from_urdf_file(URDF_PATH)
 
-    target_orientation = geometry.rpy_matrix(0, np.deg2rad(180), 0)  # point down
-    # target_orientation = geometry.rpy_matrix(0, np.deg2rad(90),0) # point forward
+	target_orientation = geometry.rpy_matrix(0, np.deg2rad(180), 0)  # point down
+	# target_orientation = geometry.rpy_matrix(0, np.deg2rad(90),0) # point forward
 
-    N_pts = 30
-    radius = 0.5
+	N_pts = 30
+	radius = 0.5
 
-    # N = 10
-    fig, ax = plot_utils.init_3d_figure()
-    ax = fig.add_subplot(111, projection="3d")
-    fig.canvas.mpl_connect("key_press_event", on_press)
+	# N = 10
+	fig, ax = plot_utils.init_3d_figure()
+	ax = fig.add_subplot(111, projection="3d")
+	fig.canvas.mpl_connect("key_press_event", on_press)
 
-    #offset = np.array([0.1, 0, 0.5])
-    #offset = np.array([0, 0, -0.5])
-    offset = np.array([0, 0, 0])
+	#offset = np.array([0.1, 0, 0.5])
+	#offset = np.array([0, 0, -0.5])
+	offset = np.array([0, 0, 0])
 
-    #pts = generate_figure_8(N=N_pts).T
-    #pts = generate_square(0.3, 20, np.array([0.3, 0.1, 0.6]))
-    #pts = generate_rectangle(N=20)
-    pts = generate_circle(N=100, radius = 0.1)
+	#pts = generate_figure_8(N=N_pts).T
+	#pts = generate_square(0.3, 20, np.array([0.3, 0.1, 0.6]))
+	#pts = generate_rectangle(N=20)
+	pts = generate_circle(N=60, radius = 0.04)
 
-    #pts *= 0.3
-    #pts *= 0.8
+	#pts *= 0.3
+	#pts *= 0.8
 
-    pts += offset
+	pts += offset
 
-    while not stopped:
+	while not stopped:
 
-        for target_position in pts:
-            if stopped:
-                break
-            ax.cla()
+		for target_position in pts:
+			if stopped:
+				break
+			ax.cla()
 
-            target_orientation = np.eye(3)
-            target_orientation[2,2] = -1 # Z-os obrnemo dol z rotacijo okrog Y - pri tem se tudi X-os obrne
-            target_orientation[0,0] = -1
-            # target_orientation = geometry.rpy_matrix(0, np.deg2rad(180), 0)  # point down
-            ik = my_chain.inverse_kinematics(target_position, target_orientation, 'all', optimizer='scalar')
-            # ik = my_chain.inverse_kinematics(target_position, target_orientation, optimizer='scalar')
+			target_orientation = np.eye(3)
+			target_orientation[2,2] = -1 # Z-os obrnemo dol z rotacijo okrog Y - pri tem se tudi X-os obrne
+			target_orientation[0,0] = -1
+			# target_orientation = geometry.rpy_matrix(0, np.deg2rad(180), 0)  # point down
+			ik = my_chain.inverse_kinematics(target_position, target_orientation, 'all', optimizer='scalar') # includes orientation
 
-            # ik = my_chain.inverse_kinematics(target_position, optimizer="scalar")  # ignores orientation
-            # ik = my_chain.inverse_kinematics(target_position, target_orientation, "all", optimizer='scalar') # includes orientation
+			# ik = my_chain.inverse_kinematics(target_position, optimizer="scalar")  # ignores orientation
 
-            ax.set_xlim(-radius, radius)
-            ax.set_ylim(-radius, radius)
-            ax.set_zlim(-radius, radius)
+			ax.set_xlim(-radius, radius)
+			ax.set_ylim(-radius, radius)
+			ax.set_zlim(-radius, radius)
 
-            my_chain.plot(ik, ax, target=target_position)
+			my_chain.plot(ik, ax, target=target_position)
 
-            plt.plot(pts[:, 0], pts[:, 1], pts[:, 2], marker=".", alpha=0.5)
+			plt.plot(pts[:, 0], pts[:, 1], pts[:, 2], marker=".", alpha=0.5)
 
-            plt.draw()
-            plt.pause(0.01)
+			plt.draw()
+			plt.pause(0.01)
 
 if __name__ == "__main__":
-    IK_demo()
+	IK_demo()
--- Vaja03/sol5b.py
+++ Vaja03/sol5b.py
@@ -18,77 +18,88 @@ from lerobot.robots.so101_follower import SO101Follower, SO101FollowerConfig
 from utils import *
 
 JOINT_NAMES = [
-    'shoulder_pan',
-    'shoulder_lift',
-    'elbow_flex',
-    'wrist_flex',
-    'wrist_roll',
-    'gripper',
+	'shoulder_pan',
+	'shoulder_lift',
+	'elbow_flex',
+	'wrist_flex',
+	'wrist_roll',
+	'gripper',
 ]
 
 def main():
-    URDF_PATH = 'so101_new_calib.urdf'
-    my_chain = ikpy.chain.Chain.from_urdf_file(URDF_PATH)
-    my_chain.active_links_mask[0]=False
+	URDF_PATH = 'so101_new_calib.urdf'
+	my_chain = ikpy.chain.Chain.from_urdf_file(URDF_PATH)
+	my_chain.active_links_mask[0]=False
 
-    # Configure robot
-    port = "/dev/arm_f4"
-    # robot_config = SO101FollowerConfig(port=port, id='arm_f1')
-    calibration_dir='calibrations/'
-    robot_config = SO101FollowerConfig(port=port, id='arm_f4', calibration_dir=Path(calibration_dir))
+	# Configure robot
+	port = "/dev/arm_f4"
+	# robot_config = SO101FollowerConfig(port=port, id='arm_f1')
+	calibration_dir='calibrations/'
+	robot_config = SO101FollowerConfig(port=port, id='arm_f4', calibration_dir=Path(calibration_dir))
 
+	robot = SO101Follower(robot_config)
+	robot.connect()
+	robot.bus.disable_torque()
 
-    robot = SO101Follower(robot_config)
-    robot.connect()
-    robot.bus.disable_torque()
-
-    # IMPORTANT for setting maximum velocity and acceleration
-    v = 500
-    a = 10
-    for j in JOINT_NAMES:
-        robot.bus.write("Goal_Velocity", j, v)
-        robot.bus.write("Acceleration", j, a)
-
-
-    offset = np.array([0, 0, 0])
-
-    # rectangle 1
-    #pts = generate_rectangle(N=60)
-    #pts += offset
+	# IMPORTANT for setting maximum velocity and acceleration
+	v = 500
+	a = 10
+	for j in JOINT_NAMES:
+		robot.bus.write("Goal_Velocity", j, v)
+		robot.bus.write("Acceleration", j, a)
 
 
-
-
-    # rectangle 2
-    #width = 0.1
-    #height = 0.05
-    #target_position=np.array([0.2, 0.0, 0.05])
-
-    #p1 = target_position
-    #p2 = target_position + [0, width, 0]
-    #p3 = target_position + [height, width, 0]
-    #p4 = target_position + [height, 0, 0]
-
-    #pts = [p1,p2,p3,p4]
+	# figure 8
+	# offset = np.array([0.2, 0, 0.2])
+	# pts = generate_figure_8(N=N_pts).T
+	# #pts = generate_square(0.3, 20, np.array([0.3, 0.1, 0.6]))
+	# pts *= 0.2
+	# pts += offset
 
 
 
 
-    # circle
-    pts = generate_circle(N=60, radius = 0.04)
+	# rectangle 1
+	# offset = np.array([0, 0, 0])
+	#pts = generate_rectangle(N=60)
+	#pts += offset
 
-    while True:
-        for pt in pts:
-            #target_orientation = np.eye(3)
-            #target_orientation[2,2] = -1 # obrne Z-os
-            #ik = my_chain.inverse_kinematics(pt, target_orientation, 'all', optimizer='scalar')
 
-            ik = my_chain.inverse_kinematics(pt, optimizer='scalar')
-            action = {JOINT_NAMES[i]+'.pos': np.rad2deg(v) for i, v in enumerate(ik[1:])}
 
-            robot.send_action(action)
-            #time.sleep(0.01)
-            #time.sleep(2)
+
+	# rectangle 2
+	#width = 0.1
+	#height = 0.05
+	#target_position=np.array([0.2, 0.0, 0.05])
+
+	#p1 = target_position
+	#p2 = target_position + [0, width, 0]
+	#p3 = target_position + [height, width, 0]
+	#p4 = target_position + [height, 0, 0]
+
+	#pts = [p1,p2,p3,p4]
+
+
+
+
+	# circle
+	pts = generate_circle(N=60, radius = 0.04)
+
+
+	while True:
+		for pt in pts:
+			# target_orientation = np.eye(3)
+			# target_orientation[2,2] = -1 # Z-os obrnemo dol z rotacijo okrog Y - pri tem se tudi X-os obrne
+			# target_orientation[0,0] = -1
+			# # target_orientation = geometry.rpy_matrix(0, np.deg2rad(180), 0)  # point down
+			# ik = my_chain.inverse_kinematics(target_position, target_orientation, 'all', optimizer='scalar') # includes orientation
+
+			ik = my_chain.inverse_kinematics(pt, optimizer='scalar')
+			action = {JOINT_NAMES[i]+'.pos': np.rad2deg(v) for i, v in enumerate(ik[1:])}
+
+			robot.send_action(action)
+			#time.sleep(0.01)
+			#time.sleep(2)
 
 if __name__=='__main__':
-    main()
+	main()
