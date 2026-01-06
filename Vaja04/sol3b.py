e739b916df312da544ae8e19cfc346305416ac3f
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2026-01-06 18:20:45 +0100

    updated sol3b to read from camera feed

--- Vaja04/sol3b.py
+++ Vaja04/sol3b.py
@@ -2,12 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import workspace_utils
-import matplotlib.patheffects as pe
 
-import warnings
-warnings.filterwarnings("ignore")
-
-import math, time
 np.set_printoptions(precision=2)
 from pathlib import Path
 
@@ -19,6 +14,7 @@ from lerobot.robots.so101_follower import SO101Follower, SO101FollowerConfig
 
 from utils import *
 
+# --------------- begin robot config
 JOINT_NAMES = [
     'shoulder_pan',
     'shoulder_lift',
@@ -48,73 +44,112 @@ a = 10
 for j in JOINT_NAMES:
     robot.bus.write("Goal_Velocity", j, v)
     robot.bus.write("Acceleration", j, a)
+# --------------- end robot config
 
-# --- Load image ---
-ime_slike = 'capture_f1.jpg'
-im = cv2.imread(ime_slike)
-im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+last_point = None
 
-# --- Compute homographies ---
-corners = workspace_utils.get_workspace_corners(im, draw_markers=True)
-H1, H2 = workspace_utils.calculate_homography_mapping(corners)
+def save_homography(im):
+    corners = workspace_utils.get_workspace_corners(im, draw_markers=True)
+    H1, H2 = workspace_utils.calculate_homography_mapping(corners)
 
-# --- Warp image to workspace (for clicking) ---
-delovna_povrsina = cv2.warpPerspective(im, H1, (1000, 1000))
+    np.savez('homography_3b.npz', H1=H1, H2=H2)
+    return H1, H2
 
-# --- Click points on the warped workspace ---
-fig, ax_click = plt.subplots(figsize=(6,6))
-ax_click.imshow(delovna_povrsina)
-ax_click.set_title("Click points on warped workspace")
-clicked_pts = plt.ginput(4, timeout=0)
-plt.close(fig)
+def load_homography():
+    data = np.load('homography_3b.npz')
+    return data['H1'], data['H2']
 
-# --- Prepare inverse of H1 to map clicks back to original image ---
-H1_inv = np.linalg.inv(H1)
+def click_event(event, x, y, flags, param):
+    global last_point
 
-# --- Plot ONLY the original image and mapped points ---
-fig, ax = plt.subplots(figsize=(8,8))
-ax.imshow(im)
-ax.set_title("Original image with robot coordinates")
+    if event == cv2.EVENT_LBUTTONDOWN:
+        last_point = np.array([x, y, 1.0])
+        send_robot_to_point(last_point)
 
-for (uw, vw) in clicked_pts:
 
-    # Step 1: warped → original image coordinates
-    pt_w = np.array([uw, vw, 1.0])
-    orig = H1_inv @ pt_w
-    orig /= orig[2]
+def send_robot_to_point(pt_im):
+    pt = H2 @ pt_im
+    pt /= pt[2]
+    print(pt)
 
-    u, v = orig[0], orig[1]
-
-    # Step 2: original image → robot coordinates
-    # pt = np.array([u, v, 1.0])
-    mapped = H2 @ pt_w
-    mapped /= mapped[2]
-
-    Xr, Yr = mapped[0], mapped[1]
-
-    pt = np.array([Xr, Yr, 0.05])
+    # dobili smo x in y; z koordinato bomo hardcodali na 0.08
+    pt[2] = 0.08
 
     ik = my_chain.inverse_kinematics(pt, optimizer='scalar')
     action = {JOINT_NAMES[i]+'.pos': np.rad2deg(v) for i, v in enumerate(ik[1:])}
 
     robot.send_action(action)
 
-    time.sleep(1)
+
+# H1, H2 = load_homography()
+H1 = H2 = None
+
+w, h = 1600, 1200
+camera_id = 0
+
+calib = np.load("wide_calibration_data.npz")
+M = calib["camera_matrix"]
+D = calib["dist_coeffs"]
+
+cap = cv2.VideoCapture(camera_id)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+
+cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+cv2.setMouseCallback("frame", click_event)
+
+while True:
+    # Poskusimo pridobiti trenutno sliko s spletne kamere
+    ret, frame = cap.read()
+
+    # Če to ni mogoče (kamera izključena, itd.), končamo z izvajanjem funkcije
+    if not ret:
+        break
+
+    undistorted = cv2.undistort(frame, M, D)
+    im = cv2.rotate(undistorted, cv2.ROTATE_180)
+
+    # če rabiš testing "brez kamere", odkomentiraj to
+    # ------ start test
+    im_path = 'capture_f1.jpg'
+    im = cv2.imread(im_path)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    # ------ end test
+
+    if H1 is None or H2 is None:
+        H1, H2 = save_homography(im)
+
+    im = cv2.warpPerspective(im, H1, (1000, 1000))
+    
+    if last_point is not None:
+        robo_point = H2 @ last_point
+        robo_point /= robo_point[2]
+
+        im_x, im_y = int(last_point[0]), int(last_point[1])
+        cv2.circle(im, (im_x, im_y), 5, (0, 0, 255), -1)
+
+        text = f"{robo_point[0]:.2f}, {robo_point[1]:.2f}"
+        # Draw black outline (thicker)
+        cv2.putText(
+            im, text, (im_x + 10, im_y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+            (0, 0, 0), 3, cv2.LINE_AA
+        )
+
+        # Draw white text on top
+        cv2.putText(
+            im, text, (im_x + 10, im_y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+            (255, 255, 255), 1, cv2.LINE_AA
+        )
+
+    cv2.imshow('frame', im)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
 
-    # # Draw point on original image
-    # ax.plot(u, v, 'yo', markersize=5)
-
-    # # Label robot coordinates
-    # ax.text(
-    #     u + 10, v - 10,
-    #     f"{Xr:.2f}, {Yr:.2f}",
-    #     color='yellow',
-    #     fontsize=10,
-    #     path_effects=[
-    #         pe.Stroke(linewidth=1, foreground='black'),
-    #         pe.Normal()
-    #     ]
-    # )
-
-# plt.show()
