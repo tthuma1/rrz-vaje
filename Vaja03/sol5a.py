2511a2cd887c95d13cc639662558b4ae35e06e23
Tim Thuma <30806431+tthuma1@users.noreply.github.com>
2026-01-07 14:54:05 +0100

    testing orientations

--- Vaja03/sol5a.py
+++ Vaja03/sol5a.py
@@ -36,7 +36,7 @@ def IK_demo():
 
 	# N = 10
 	fig, ax = plot_utils.init_3d_figure()
-	ax = fig.add_subplot(111, projection="3d")
+	# ax = fig.add_subplot(111, projection="3d")
 	fig.canvas.mpl_connect("key_press_event", on_press)
 
 	#offset = np.array([0.1, 0, 0.5])
@@ -46,7 +46,8 @@ def IK_demo():
 	#pts = generate_figure_8(N=N_pts).T
 	#pts = generate_square(0.3, 20, np.array([0.3, 0.1, 0.6]))
 	#pts = generate_rectangle(N=20)
-	pts = generate_circle(N=60, radius = 0.04)
+	pts = generate_circle(N=60, radius = 0.08)
+	# pts[:,2] = 0.5*(abs(pts[:,0]))
 
 	#pts *= 0.3
 	#pts *= 0.8
@@ -54,13 +55,13 @@ def IK_demo():
 
 
 	# horizontal line
-	ys1 = np.linspace(-0.15, 0.15, 30)
-	ys2 = np.linspace(0.15, -0.15, 30)
-	ys = np.concatenate((ys1, ys2), axis=None)
-	pts = np.column_stack([np.zeros_like(ys), ys, np.zeros_like(ys)])
-	offset = np.array([0.4, 0, 0.08])
+	# ys1 = np.linspace(-0.15, 0.15, 30)
+	# ys2 = np.linspace(0.15, -0.15, 30)
+	# ys = np.concatenate((ys1, ys2), axis=None)
+	# pts = np.column_stack([np.zeros_like(ys), ys, np.zeros_like(ys)])
+	# offset = np.array([0.4, 0, 0.08])
 
-	pts += offset
+	# pts += offset
 
 	while not stopped:
 
@@ -69,17 +70,25 @@ def IK_demo():
 				break
 			ax.cla()
 
-			target_orientation = np.eye(3)
-			target_orientation[2,2] = -1 # Z-os obrnemo dol z rotacijo okrog Y - pri tem se tudi X-os obrne
-			target_orientation[0,0] = -1
-			# target_orientation = geometry.rpy_matrix(0, np.deg2rad(180), 0)  # point down
+			# target_orientation = np.eye(3)
+			# target_orientation[2,2] = -1 # Z-os obrnemo dol z rotacijo okrog Y - pri tem se tudi X-os obrne
+			# target_orientation[0,0] = -1
+			target_orientation = geometry.rpy_matrix(0, np.deg2rad(180), 0)  # point down
+			# ik = my_chain.inverse_kinematics(target_position, target_orientation[:,0], 'X', optimizer='scalar') # includes orientation
+			ik = my_chain.inverse_kinematics(target_position, target_orientation[:,2], 'Z', optimizer='scalar') # includes orientation
+
+			# target_orientation = geometry.rpy_matrix(0, np.deg2rad(90), 0)  # point down
 			# ik = my_chain.inverse_kinematics(target_position, target_orientation, 'all', optimizer='scalar') # includes orientation
 
-			ik = my_chain.inverse_kinematics(target_position, optimizer="scalar")  # ignores orientation
+			# ik = my_chain.inverse_kinematics(target_position, optimizer="scalar")  # ignores orientation
 
 			ax.set_xlim(-radius, radius)
 			ax.set_ylim(-radius, radius)
 			ax.set_zlim(-radius, radius)
+
+			ax.set_xlabel("X")
+			ax.set_ylabel("Y")
+			ax.set_zlabel("Z")
 
 			my_chain.plot(ik, ax, target=target_position)
 
